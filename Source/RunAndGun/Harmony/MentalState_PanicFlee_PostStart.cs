5d1a1d7811a6bf0c55b891db5b02a6af8a59d6ab
Roel <roelheirman@gmail.com>
2018-02-08 10:59:33 +0100

    verbose logging

--- Source/RunAndGun/Harmony/MentalState_PanicFlee_PostStart.cs
+++ Source/RunAndGun/Harmony/MentalState_PanicFlee_PostStart.cs
@@ -13,27 +13,38 @@ namespace RunAndGun.Harmony
 {
 
     [HarmonyPatch(typeof(MentalState_PanicFlee), "PostStart")]
-    static class MentalState_PanicFlee_PostStart
+    public class MentalState_PanicFlee_PostStart
     {
         static void Postfix(MentalState __instance)
         {
+            Log.Message("1");
             CompRunAndGun comp = __instance.pawn.TryGetComp<CompRunAndGun>();
-            if (comp != null && Base.enableRGForAI.Value)
+            Log.Message("2");
+
+            if (comp != null && RunAndGun.Base.enableRGForAI.Value)
             {
+                Log.Message("3");
+
                 comp.isEnabled = shouldRunAndGun();
+                Log.Message("9");
 
             }
         }
         static bool shouldRunAndGun()
         {
+            Log.Message("4");
             var rndInt = new Random(DateTime.Now.Millisecond).Next(1, 100);
-            int chance = Base.enableRGForFleeChance.Value;
+            Log.Message("5");
+            int chance = RunAndGun.Base.enableRGForFleeChance.Value;
+            Log.Message("6");
             if (rndInt <= chance)
             {
+                Log.Message("7");
                 return true;
             }
             else
             {
+                Log.Message("8");
                 return false;
             }
 
