3e5dea60a410e52f7baa3cbe4e4c671f075ffbb3
rheirman <roelheirman@gmail.com>
2022-10-24 11:41:07 +0200

    1.4 update

---
+++ 1.4/Source/RunAndGun/CompProperties_RunAndGun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RunAndGun
+{
+    class CompProperties_RunAndGun : CompProperties
+    {
+        public CompProperties_RunAndGun()
+        {
+            compClass = typeof(CompRunAndGun);
+        }
+    }
+}
