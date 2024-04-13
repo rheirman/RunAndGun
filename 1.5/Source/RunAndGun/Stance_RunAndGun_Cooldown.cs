4a037df77e4d845f8307250596a38e474827d423
BlackFox <106489375+BlackFox-GH@users.noreply.github.com>
2024-04-13 20:30:32 +0200

    1.5 compatibility
    This should be everything you need to make it compatible with the new version.

---
+++ 1.5/Source/RunAndGun/Stance_RunAndGun_Cooldown.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+using RimWorld;
+
+namespace RunAndGun
+{
+    class Stance_RunAndGun_Cooldown : Stance_Cooldown
+    {
+        public override bool StanceBusy
+        {
+            get
+            {
+                return Pawn != null && Pawn.CurJob != null && Pawn.CurJob.def != JobDefOf.Goto;
+            }
+        }
+        public Stance_RunAndGun_Cooldown()
+        {
+        }
+        public Stance_RunAndGun_Cooldown(int ticks, LocalTargetInfo focusTarg, Verb verb) : base(ticks, focusTarg, verb)
+        {
+        }
+
+    }
+
+}
